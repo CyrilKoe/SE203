41d209e9b08f9eb9c179360d192177a7824fa5a3
Cyril KOENIG <cyril.koenig@telecom-paristech.fr>
2019-09-30 16:49:37 +0200

    Initialisation du .bss

--- TD/init.c
+++ TD/init.c
@@ -1,4 +1,10 @@
+#include <stdint.h>
+extern uint8_t _bss_start, _bss_end;
+
+//Remplit .bss à 0 octet par octet
 void init_bss()
 {
-  return;
+  uint8_t *dst;
+  for (dst = &_bss_start; dst < &_bss_end; dst++)
+    *dst = 0;
 }
