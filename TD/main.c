00555251af4080c3b580a84ed5d1263a2e2dfdb9
Cyril KOENIG <cyril.koenig@telecom-paristech.fr>
2019-09-30 14:48:51 +0200

    Linker script valide avec ENTRY ok

---
+++ TD/main.c
@@ -0,0 +1,18 @@
+int a = 8;
+
+int fibo(int);
+
+
+int main()
+{
+  return fibo(8);
+}
+
+int fibo(int n){
+  switch (n)
+  {
+    case 1: return 1;
+    case 2: return 1;
+    default: return fibo(n-1) + fibo(n-2);
+  }
+}
