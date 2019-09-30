caa8ae05b8610eb5ac7eee5cc8c8beb5af2e1dc0
Cyril KOENIG <cyril.koenig@telecom-paristech.fr>
2019-09-30 14:50:59 +0200

    Ecriture du code de fibonacci

--- TD/main.c
+++ TD/main.c
@@ -1,6 +1,18 @@
-int a = 0;
-int b = 2,
+int a = 8;
+
+int fibo(int);
+
+
 int main()
 {
-  while(1){}
+  return fibo(8);
+}
+
+int fibo(int n){
+  switch (n)
+  {
+    case 1: return 1;
+    case 2: return 1;
+    default: return fibo(n-1) + fibo(n-2);
+  }
 }
