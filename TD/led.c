4380cd5bbf1637fa597af26d30bd7b4c55000d41
Cyril KOENIG <cyril.koenig@telecom-paristech.fr>
2019-10-04 17:35:24 +0200

    Fonctions led on et led off

--- TD/led.c
+++ TD/led.c
@@ -1,11 +1,26 @@
+#include <stdint.h>
+
+
 #define RCC_AHB2ENR (*(volatile uint32_t *) 0x4002104C)
-#define GPIOB_MODER (*(volatile uint32_t *) 0x4800 0400)
+#define GPIOB_MODER (*(volatile uint32_t *) 0x48000400)
+#define GPIOB_BSRR (*(volatile uint32_t *) 0x48000418)
+#define GPIOB_ODR (*(volatile uint32_t *) 0x48000414)
 
 /* Active l'horloge du bloc GPIO B
 (Place dans le registre RCC_AHB2ENR la valeur 1 au bit 1 (GPIOBEN))
 Met la broche 15 du bloc GPIO B en mode sortie
 */
 void led_init() {
-  RCC_AHB2ENR = (1 << 1);
-  GPIOB_MODER = (01 << (15*2));
+  RCC_AHB2ENR |= (1 << 1);
+  GPIOB_MODER = 0xDFFFFEBF;
+}
+
+// Place le bit 1 sur la partie SET
+void led_on() {
+  GPIOB_BSRR = 0b0100000000000000;
+}
+
+// Place le bit 1 sur la partie RESET
+void led_off() {
+  GPIOB_BSRR = 0b0100000000000000 << 16;
 }
--- TD/led.h
+++ TD/led.h
@@ -2,5 +2,8 @@
 #define LED_H
 
 void led_init();
+void led_on();
+void led_off();
+
 
 #endif
