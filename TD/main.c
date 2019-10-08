79fee3456424569a0e688dcda1657ad2e2481186
Cyril Koenig <cyril.koenig@telecom-paristech.fr>
2019-10-08 13:51:27 +0200

    Analyse de ce qui est reçu par checksum

--- TD/main.c
+++ TD/main.c
@@ -1,32 +1,35 @@
 #include "led.h"
 #include "uart.h"
 #include "clocks/clocks.h"
+#include <string.h>
 #include <math.h>
 
 int fibo(int);
 void wait(unsigned int n);
 void init_all();
-int32_t do_sum(uint8_t *s, int32_t a);
-int32_t pow_i(int32_t a, int32_t n);
+
 
 int main()
 {
   init_all();
 
-  uint8_t g[80];
+  uint8_t g[30][10];
 
   int a = 0;
 
-  while(1) {
-    uart_gets(g, sizeof(g));
-    uart_puts(g);
+  for(uint8_t i = 0; i<30; i++) {
+    uart_gets(g[i], sizeof(g[i]));
     a++;
     if(a%2)
       led_on();
     else
       led_off();
   }
-  
+
+  for(uint8_t i = 0; i<30; i++) {
+    uart_puts(g[i]);
+  }
+
   return 0;
 }
 
@@ -41,27 +44,6 @@ int fibo(int n){
     return fibo(n-1) + fibo(n-2);
   else
     return 1;
-}
-
-int32_t do_sum(uint8_t *s, int32_t a) {
-  int32_t sum = 0;
-  uint8_t k = 0;
-  do{
-    sum += (*s-'0')*pow_i(10,k);
-    s++;
-    k++;
-  } while(*s >= '0' && *s <= '9');
-  sum += a;
-  return sum;
-}
-
-int32_t pow_i(int32_t a, int32_t n) {
-  int32_t r = 1;
-  while (n > 0) {
-    r *= a;
-    n--;
-  }
-  return r;
 }
 
 void wait(unsigned int n){
