a6a29ac2d2acb72d3550cbdc183dd7c52ea17275
Cyril KOENIG <cyril.koenig@telecom-paristech.fr>
2019-10-11 10:32:35 +0200

    Ajout sendbyte

--- TD/matrix.c
+++ TD/matrix.c
@@ -5,7 +5,23 @@ SB=PC5 LAT=PC4 RST=PC3 SCK=PB1 SDA=PA4
 C0=PB2 C1=PA15 C2=PA2 C3=PA7 C4=PA6 C5=PA5 C6=PB0 C7=PA3
 */
 
+#define RST(x) = GPIOC -> BSRR = GPIO_BSRR_BR3 >> (16*x)
+#define SB(x) = GPIOC -> BSRR = GPIO_BSRR_BR5 >> (16*x)
+#define LAT(x) = GPIOC -> BSRR = GPIO_BSRR_BR4 >> (16*x)
+#define SDA(x) = GPIOA -> BSRR = GPIO_BSRR_BR4 >> (16*x)
+#define ROW0(x) = GPIOB -> BSRR = GPIO_BSRR_BR2 >> (16*x)
+#define ROW1(x) = GPIOA -> BSRR = GPIO_BSRR_BR15 >> (16*x)
+#define ROW2(x) = GPIOA -> BSRR = GPIO_BSRR_BR2 >> (16*x)
+#define ROW3(x) = GPIOA -> BSRR = GPIO_BSRR_BR7 >> (16*x)
+#define ROW4(x) = GPIOA -> BSRR = GPIO_BSRR_BR6 >> (16*x)
+#define ROW5(x) = GPIOA -> BSRR = GPIO_BSRR_BR5 >> (16*x)
+#define ROW6(x) = GPIOB -> BSRR = GPIO_BSRR_BR0 >> (16*x)
+#define ROW7(x) = GPIOA -> BSRR = GPIO_BSRR_BR3 >> (16*x)
+#define pulse_SCK = do{SCK(0); wait(3); SCK(1); wait(3); SCK(0); wait(3)}while()
+#define pulse_LAT = do{LAT(1); wait(3); LAT(0); wait(3); LAT(1); wait(3)}while()
+
 static void wait(unsigned int n);
+void send_byte(uint8_t val, int bank);
 
 void matrix_init() {
     //Enable clock GPIOB  et USART1
@@ -34,6 +50,15 @@ void matrix_init() {
     wait(10000);
     //RST = 1
     GPIOC -> BSRR = GPIO_BSRR_BS3;
+}
+
+void send_byte(uint8_t val, int bank){
+    SB(bank);
+    for(int k = 7; k>=0; i++) {
+        SDA((val >> k) & 1);
+        wait(1);
+        pulse_SCK();
+    }
 }
 
 static void wait(unsigned int n){
